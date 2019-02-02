5479a7b1862a0d5ecf7aaa17236cf264bc326784
AtanasK <atanasmk16@gmail.com>
2019-02-02 19:03:21 +0100

    Adds new chart and methods for creating data for it

--- ingredients_per_recipe.py
+++ ingredients_per_recipe.py
@@ -38,4 +38,40 @@ def write_region_averages():
         csv_writer.writerows(region_average_list)
 
 
-write_region_averages()  # execute writing to csv
+def get_ingredient_per_recipe():
+    ingredient_per_recipe = {}
+    region_dict = get_region_dict()
+
+    for region in region_dict.keys():
+        ingredient_per_recipe.setdefault(region, [])
+        num_ingredients = list(set(region_dict[region]))  # get unique numbers only
+        for num in num_ingredients:
+            count = region_dict[region].count(num)
+            ingredient_per_recipe[region].append((num, count))
+
+    list_result = []
+    for region in ingredient_per_recipe.keys():
+        for item in ingredient_per_recipe[region]:
+            list_result.append(
+                (region, item[0], item[1])
+            )
+
+    return list_result
+
+
+def write_ingredient_per_recipe():
+    ingredient_per_recipe = get_ingredient_per_recipe()
+    with open('ingredient_per_recipe.csv', mode='w', newline='') as outfile:
+        csv_writer = csv.writer(outfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow(['Region', ])
+        csv_writer.writerows(ingredient_per_recipe)
+
+
+# write_region_averages()  # execute writing to csv
+write_ingredient_per_recipe()  # execute writing ingredient per recipe to csv
+
+exit(0)
+
+from pprint import pprint
+
+pprint(get_ingredient_per_recipe())
